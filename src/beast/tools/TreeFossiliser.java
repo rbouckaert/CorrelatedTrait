26b1ff060ffa4611d89917fee3cdeb7153fb6d16
rbouckaert <higgs.ml@gmail.com>
2020-06-29 07:40:16 +1200

    remove Babel dependency

--- src/beast/tools/TreeFossiliser.java
+++ src/beast/tools/TreeFossiliser.java
@@ -1,17 +1,21 @@
 package beast.tools;
 
+
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
-import babel.tools.TreeCombiner;
 import beast.app.beauti.BeautiDoc;
 import beast.app.treeannotator.TreeAnnotator;
 import beast.app.treeannotator.TreeAnnotator.MemoryFriendlyTreeSet;
 import beast.app.util.Application;
+import beast.app.util.OutFile;
+import beast.app.util.TreeFile;
 import beast.core.Description;
 import beast.core.Input;
 import beast.core.util.Log;
@@ -21,11 +25,18 @@ import beast.evolution.tree.Tree;
 
 @Description("Grafts nodes into a tree at specified clades with zero branch lengths in order to fossilise clade values "
 		+ "in ancestral reconstruction analyses using the TreeSetLikelihood.")
-public class TreeFossiliser extends TreeCombiner {
+public class TreeFossiliser extends beast.core.Runnable {
 	final public Input<File> cfgFileInput = new Input<>("cfg", "tab separated configuration file containing two columns: "
 			+ "column 1: name of taxon\n"
 			+ "column 2: a comma separated list of taxa determining MRCA to graft above in source tree (if no constraints have been specified).");
+	final public Input<TreeFile> srcInput = new Input<>("src","source tree (set) file used as skeleton");
+	final public Input<OutFile> outputInput = new Input<>("out", "output file, or stdout if not specified",
+			new OutFile("[[none]]"));
+
+    boolean [] nodesTraversed;
+    int nseen;
 	
+ 	
 	String [] taxonName;
 	Set<String> [] subTaxonSets;
 	
@@ -118,6 +129,87 @@ public class TreeFossiliser extends TreeCombiner {
 		}
 	}
 
+	   protected Node getCommonAncestor(Node n1, Node n2) {
+	        // assert n1.getTree() == n2.getTree();
+	        if( ! nodesTraversed[n1.getNr()] ) {
+	            nodesTraversed[n1.getNr()] = true;
+	            nseen += 1;
+	        }
+	        if( ! nodesTraversed[n2.getNr()] ) {
+	            nodesTraversed[n2.getNr()] = true;
+	            nseen += 1;
+	        }
+	        while (n1 != n2) {
+		        double h1 = n1.getHeight();
+		        double h2 = n2.getHeight();
+		        if ( h1 < h2 ) {
+		            n1 = n1.getParent();
+		            if( ! nodesTraversed[n1.getNr()] ) {
+		                nodesTraversed[n1.getNr()] = true;
+		                nseen += 1;
+		            }
+		        } else if( h2 < h1 ) {
+		            n2 = n2.getParent();
+		            if( ! nodesTraversed[n2.getNr()] ) {
+		                nodesTraversed[n2.getNr()] = true;
+		                nseen += 1;
+		            }
+		        } else {
+		            //zero length branches hell
+		            Node n;
+		            double b1 = n1.getLength();
+		            double b2 = n2.getLength();
+		            if( b1 > 0 ) {
+		                n = n2;
+		            } else { // b1 == 0
+		                if( b2 > 0 ) {
+		                    n = n1;
+		                } else {
+		                    // both 0
+		                    n = n1;
+		                    while( n != null && n != n2 ) {
+		                        n = n.getParent();
+		                    }
+		                    if( n == n2 ) {
+		                        // n2 is an ancestor of n1
+		                        n = n1;
+		                    } else {
+		                        // always safe to advance n2
+		                        n = n2;
+		                    }
+		                }
+		            }
+		            if( n == n1 ) {
+	                    n = n1 = n.getParent();
+	                } else {
+	                    n = n2 = n.getParent();
+	                }
+		            if( ! nodesTraversed[n.getNr()] ) {
+		                nodesTraversed[n.getNr()] = true;
+		                nseen += 1;
+		            } 
+		        }
+	        }
+	        return n1;
+	    }
+
+		protected Node getMRCA(Tree tree, Set<String> taxa) {
+			List<Node> leafs = new ArrayList<>();
+			for (Node node : tree.getExternalNodes()) {
+				if (taxa.contains(node.getID())) {
+					leafs.add(node);
+				}
+			}
+
+	        nodesTraversed = new boolean[tree.getNodeCount()];
+	        Node cur = leafs.get(0);
+
+	        for (int k = 1; k < leafs.size(); ++k) {
+	            cur = getCommonAncestor(cur, leafs.get(k));
+	        }
+			return cur;
+		}
+		
 
 
 
